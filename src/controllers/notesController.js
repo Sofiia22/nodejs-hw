59f6527d0cb37ab7aa0de03fe5c3a45d5997280d
Sofiia Maksymchuk <sofiiamaksymchuk@Sofiias-MacBook-Air.local>
2026-06-10 23:53:45 -0400

    Add validation pagination and search

--- src/controllers/notesController.js
+++ src/controllers/notesController.js
@@ -2,9 +2,37 @@ import createHttpError from 'http-errors';
 import { Note } from '../models/note.js';
 
 export const getAllNotes = async (req, res) => {
-  const notes = await Note.find();
+  const { page = 1, perPage = 10, tag, search } = req.query;
 
-  res.status(200).json(notes);
+  const skip = (page - 1) * perPage;
+
+  const query = Note.find();
+
+  if (tag) {
+    query.where({ tag });
+  }
+
+  if (search !== undefined) {
+    query.where({
+      $or: [
+        { title: { $regex: search, $options: 'i' } },
+        { content: { $regex: search, $options: 'i' } },
+      ],
+    });
+  }
+
+  const totalNotes = await Note.countDocuments(query.getFilter());
+  const notes = await query.skip(skip).limit(perPage);
+
+  const totalPages = Math.ceil(totalNotes / perPage);
+
+  res.status(200).json({
+    page,
+    perPage,
+    totalNotes,
+    totalPages,
+    notes,
+  });
 };
 
 export const getNoteById = async (req, res) => {
--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 import dotenv from 'dotenv';
 import express from 'express';
 import cors from 'cors';
+import { errors } from 'celebrate';
 
 import { connectMongoDB } from './db/connectMongoDB.js';
 import { logger } from './middleware/logger.js';
@@ -21,6 +22,8 @@ app.use(cors());
 app.use(notesRoutes);
 
 app.use(notFoundHandler);
+
+app.use(errors());
 
 app.use(errorHandler);
 
