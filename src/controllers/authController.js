0e978958ebb970828900fc087210afd59b35c3a0
Sofiia Maksymchuk <sofiiamaksymchuk@Sofiias-MacBook-Air.local>
2026-06-22 14:25:32 -0400

    fix mentor review issues

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -93,6 +93,12 @@ export const refreshUserSession = async (req, res, next) => {
     }
 
     if (new Date() > new Date(session.refreshTokenValidUntil)) {
+      await Session.deleteOne({
+        _id: session._id,
+      });
+
+      clearSessionCookies(res);
+
       throw createHttpError(401, 'Session token expired');
     }
 
