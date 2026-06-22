0188847e77202c35377d62831ef37589ddaeb5c7
Sofiia Maksymchuk <sofiiamaksymchuk@Sofiias-MacBook-Air.local>
2026-06-22 14:16:38 -0400

    add authentication and user sessions

--- src/routes/notesRoutes.js
+++ src/routes/notesRoutes.js
@@ -1,6 +1,6 @@
 import { Router } from 'express';
 import { celebrate } from 'celebrate';
-
+import { authenticate } from '../middleware/authenticate.js';
 import {
   createNote,
   deleteNote,
@@ -17,6 +17,7 @@ import {
 } from '../validations/notesValidation.js';
 
 const router = Router();
+router.use(authenticate);
 
 router.get('/notes', celebrate(getAllNotesSchema), getAllNotes);
 
--- src/server.js
+++ src/server.js
@@ -1,12 +1,16 @@
 import dotenv from 'dotenv';
 import express from 'express';
 import cors from 'cors';
+import cookieParser from 'cookie-parser';
 import { errors } from 'celebrate';
 
 import { connectMongoDB } from './db/connectMongoDB.js';
+
 import { logger } from './middleware/logger.js';
 import { notFoundHandler } from './middleware/notFoundHandler.js';
 import { errorHandler } from './middleware/errorHandler.js';
+
+import authRoutes from './routes/authRoutes.js';
 import notesRoutes from './routes/notesRoutes.js';
 
 dotenv.config();
@@ -16,14 +20,18 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(logger);
+
 app.use(express.json());
+app.use(cookieParser());
+
 app.use(cors());
 
+app.use('/auth', authRoutes);
 app.use(notesRoutes);
 
-app.use(notFoundHandler);
-
 app.use(errors());
+
+app.use(notFoundHandler);
 
 app.use(errorHandler);
 
